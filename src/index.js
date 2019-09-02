40d25da483ebccc3a3f49d4c7c46e46b9cca9e58
yooseok <rdt419@gmail.com>
2019-09-02 19:06:42 +0900

    index restapi 추가

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const app = express();
 
+let users=[];
+let user = null;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 app.listen(3000);
 
-app.get('/', (req, res) =>{
-    let a=5;
-    let b=5;
-    let c=a+b;
-    res.send(c.toString());
-    console.log('server on');
+app.get('/users', (req, res) =>{
+    res.send("user"+user.name+'get');
+});
+
+app.get('/users/:id',(req, res)=>{
+    if(user.id == req.params.id){
+        res.send('user'+user.name+'get');
+    }
+    res.send('user id'+req.param.id+'존재하지 않습니다');
+});
+
+app.post('/users',(req, res)=>{
+    user =req.body;
+    res.send('user '+user.name+'추가');
+});
+
+app.put('/users/:name',(req, res)=>{
+    user = req.body;
+    if(user.name == req.params.name){
+        res.send('user 이름 '+user.name+' 수정');
+    }
+    res.send('user name'+req.param.name+'존재하지 않습니다');
+});
+
+
+app.delete('/users/:id',(req, res)=>{
+    user= req.body;
+   
+    if(user.id == req.params.id){
+        res.send('user delete'+user.id);
+    }
+    res.send('user id'+req.param.id+'존재하지 않습니다');
+   
 });
