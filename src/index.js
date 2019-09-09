f9fabf83f2f6b32a98c35d6cc12925ec91c8e300
yooseok <rdt419@gmail.com>
2019-09-09 14:19:50 +0900

    update부분 설계

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const _ = require("lodash");
 const app = express();
 
-let users=[];
+let users=[{
+    id : 1,
+    name: '홍길동'
+},{
+    id : 2,
+    name : '장형수'
+}];
 let user = null;
 
 app.use(express.json());
@@ -10,36 +17,51 @@ app.use(express.urlencoded({extended:true}));
 app.listen(3000);
 
 app.get('/users', (req, res) =>{
-    res.send("user"+user.name+'get');
+let msg = '유저가 존재하지 않습니다';
+if(users.length>0){
+    msg = users.length+'명의 유저가 존재합니다';
+}
+res.send({msg, result: users});
 });
 
 app.get('/users/:id',(req, res)=>{
-    if(user.id == req.params.id){
-        res.send('user'+user.name+'get');
-    }
-    res.send('user id'+req.param.id+'존재하지 않습니다');
+ let msg = 'id가' +req.params.id+"인 유저가 존재하지 않습니다";
+ let user = _.find(users, ["id", parseInt(req.params.id)]);
+ if(user){
+     msg = '성공적으로 조회하였습니다';
+ }
+ res.send({msg, result: user});
 });
 
 app.post('/users',(req, res)=>{
-    user =req.body;
-    res.send('user '+user.name+'추가');
+   const check_user = _.find(users,['id', users.id]);
+   let msg = req.body.id+'아이디를 가진 유저가 이미 존재합니다.';
+   let success = false;
+   if(!check_user){
+       users.push(req.body);
+       msg = req.body.name +' 유저를 새로 추가했습니다';
+       success = true;
+   }
+   res.send({msg, success});
 });
 
-app.put('/users/:name',(req, res)=>{
-    user = req.body;
-    if(user.name == req.params.name){
-        res.send('user 이름 '+user.name+' 수정');
+app.put('/users/:id',(req, res)=>{
+    msg = '유저의 이름을 '+req.body.name+' 으로 수정하였습니다'
+    if(users.id === req.body.id){
+        users.name = req.body.name;
+    }else{
+        msg = '존재하지 않는 유저입니다.';
     }
-    res.send('user name'+req.param.name+'존재하지 않습니다');
+    res.send(msg);
 });
 
 
 app.delete('/users/:id',(req, res)=>{
-    user= req.body;
-   
     if(user.id == req.params.id){
+        user = null;
         res.send('user delete'+user.id);
+    }else{
+    res.send('user id'+req.param.id+' 존재하지 않습니다');
     }
-    res.send('user id'+req.param.id+'존재하지 않습니다');
    
 });
