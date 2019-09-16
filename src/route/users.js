2785d340bae612d2097f6b5db91eb147d2450ae1
yooseok <rdt419@gmail.com>
2019-09-16 14:06:19 +0900

    user mysql 초기화

--- src/route/users.js
+++ src/route/users.js
@@ -1,6 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("node_example", "root", "903956", {host : "localhost", dialect:"mysql"});
+
+const check_sequelize_auth = async () =>{
+    try {
+        await sequelize.authenticate();
+        console.log("연결성공");
+    } catch (error) {
+        console.log("연결실패", err);
+    }
+};
+
+check_sequelize_auth();
+
+const User = sequelize.define("user",{
+    name:{
+        type: Sequelize.STRING,
+        allowNull:false
+    },
+    password:{
+        type: Sequelize.INTEGER,
+        allowNull:false
+    }
+});
+
+User.sync({force : true}).then(()=>{
+    return User.create({
+        name : "홍길동",
+        password : '1234'
+    });
+});
 
 let users=[{
     id : 1,
@@ -12,68 +43,85 @@ let users=[{
 
 
 
-router.get('/', (req, res) =>{
-let msg = '유저가 존재하지 않습니다';
-if(users.length>0){
-    msg = users.length+'명의 유저가 존재합니다';
-}
-res.send({msg, result: users});
-});
-
-router.get('/:id',(req, res)=>{
- let msg = 'id가' +req.params.id+"인 유저가 존재하지 않습니다";
- let user = _.find(users, ["id", parseInt(req.params.id)]);
- if(user){
-     msg = '성공적으로 조회하였습니다';
- }
- res.send({msg, result: user});
-});
-
-router.post('/',(req, res)=>{
-   const check_user = _.find(users,['id', parseInt(req.params.id)]);
-   let msg = req.body.id+'아이디를 가진 유저가 이미 존재합니다.';
-   let success = false;
-   if(!check_user){
-       users.push(req.body);
-       msg = req.body.name +' 유저를 새로 추가했습니다';
-       success = true;
-   }
-   res.send({msg, success});
-});
-
-router.put('/:id',(req, res)=>{
-    const check_user = users.map((entry)=>{
-        if(entry.id === parseInt(req.params.id)){
-            entry = true;
-        }else{
-            entry = false;
+router.get('/address/:password', async(req, res) =>{
+    let result = await User.findAll({
+        where:{
+            password : req.params.password
         }
-        return entry;
-    })
-    msg = '유저를 찾을수 없습니다';
+    });
+    res.send(result);
+});
+
+router.get('/:id',async(req, res)=>{
+    let result = await User.findOne({
+        attributes:["name"],
+        where : {
+            id:req.params.id
+        }
+    });
+
+    res.send(result);
+});
+
+router.post('/',async(req, res)=>{
+   let result = false;
+   try {
+       await User.create({
+           id:req.body.id, name: req.body.name, password : req.body.password
+       });
+   } catch (error) {
+       console.log(error);
+   }
+   console.log(result);
+});
+
+router.put('/:id',async(req, res)=>{
+    // const check_user = await User.findOne({
+    //     where:{
+    //         password:req.params.id
+    //     }
+    // });
     
-    if(check_user){
-       users= users.map((entry)=>{
-            if(entry.id === parseInt(req.params.id)){
-                entry.name = req.body.name;
-                msg = '유저의 이름을 '+req.body.name+' 으로 수정하였습니다';
+    let msg;
+    
+        
+    try {
+        await User.update({
+            name : req.body.name
+        }, {
+            where:{
+                password:req.params.id
             }
-            return entry;
-        })
+        });
+        msg = '유저를 찾아서 '+req.body.name+' 으로 바뀌었습니다.';
+    } catch (error) {
+        console.log("유저를 찾을수 없습니다",error);
     }
     res.send(msg);
-});
-
-
-router.delete('/:id',(req, res)=>{
-    let check_user = _.find(users,["id", parseInt(req.params.id)]);
-    let msg = req.params.id+" 아이디를 가진 유저가 존재하지 않습니다";
-    if(check_user){
-        msg = '성공적으로 삭제하였습니다';
-        // users = _.reject(users, ["id", parseInt(req.params.id)]);
-        
     }
-   res.send(msg);
+    
+
+);
+
+
+router.delete('/:id',async(req, res)=>{
+    
+    
+    let mag;
+   
+        
+        try {
+            await User.destroy({
+                where:{
+                    password:req.params.id
+                }
+            });
+           mag = req.params.id+" 를 가진 유저를 삭제하였습니다."
+        } catch (error) {
+            console.log(error);
+        }
+    
+   res.send(mag);
 });
 
 module.exports = router;
